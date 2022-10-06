b052d4f72c4ce205b05e62ddbfa596315cd06cef
Philipp-Borchert-ISH <Philipp-Borchert-ISH@users.noreply.github.com>
2022-10-06 08:18:55 +0200

    fix: cleanup sample functions (#16)

--- src/main/resources/archetype-resources/src/sql-config/dbmigrate/200_add_config_parameter.sql
+++ src/main/resources/archetype-resources/src/sql-config/dbmigrate/200_add_config_parameter.sql
@@ -1,27 +1,34 @@
-DO $$
+
+
+-- DROP FUNCTION oms.add_config_parameter(bigint, character varying, character varying, character varying);
+CREATE OR REPLACE FUNCTION oms.add_s2s_config_parameter(
+	 p_shop2supplierref bigint,
+	 p_configtype       varchar,
+	 p_key              varchar,
+	 p_value            varchar)
+  RETURNS void AS
+$$
+
 BEGIN
 
-		-- DROP FUNCTION oms.add_config_parameter(bigint, character varying, character varying, character varying);
-		CREATE OR REPLACE FUNCTION oms.add_config_parameter(
-		    p_shop2supplierref bigint,
-		    p_configtype character varying,
-		    p_key character varying,
-		    p_value character varying)
-		  RETURNS void AS
-		$BODY$BEGIN
-		  IF NOT EXISTS (SELECT 1 FROM oms."CustomConfigurationDO"  where "shop2SupplierRef" = p_shop2supplierref and "configType" = p_configtype) THEN
-		    INSERT INTO oms."CustomConfigurationDO" ("id", "shop2SupplierRef", "configType")
-		      SELECT nextval('oms."CustomConfigurationDO_id_seq"'), p_shop2supplierref, p_configtype;
-		  END IF;
-		  IF p_key IS NOT NULL THEN
-			  insert into oms."CustomConfigurationDO_AV" ("customConfigurationRef", key, value)
-				select (select id from oms."CustomConfigurationDO" where "shop2SupplierRef" = p_shop2supplierref and "configType" = p_configtype), p_key, p_value
-				ON CONFLICT ("customConfigurationRef", key) DO UPDATE SET value = p_value;
-		  END IF;
-		END;
-		$BODY$
-		  LANGUAGE plpgsql VOLATILE
-		  COST 100;
+	INSERT INTO oms."CustomConfigurationDO" ("id", "shop2SupplierRef", "configType")
+	SELECT nextval('oms."CustomConfigurationDO_id_seq"'), p_shop2supplierref, p_configtype
+	WHERE NOT EXISTS (select 1 FROM oms."CustomConfigurationDO"  
+					      where "shop2SupplierRef" = p_shop2supplierref and "configType" = p_configtype);
 
+	IF p_key IS NOT NULL THEN
+		INSERT INTO oms."CustomConfigurationDO_AV" ("customConfigurationRef", key, value)
+		SELECT (select id from oms."CustomConfigurationDO" where "shop2SupplierRef" = p_shop2supplierref and "configType" = p_configtype), p_key, p_value
+		ON CONFLICT ("customConfigurationRef", key) DO UPDATE SET value = p_value;
+	END IF;
+  
 END;
-$$;
+
+$$
+LANGUAGE plpgsql VOLATILE;
+
+comment on function oms.add_s2s_config_parameter(
+     p_shop2supplierref bigint,
+     p_configtype       varchar,
+     p_key              varchar,
+     p_value            varchar) is 'add config values for a given Shop2SupplierDO / configType';
